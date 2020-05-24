1e4954e7b2a48f28dbc21c3337630c923a1c5af6
scd7896 <scd1212@naver.com>
2020-05-24 15:26:55 +0900

    미로찾기 계산 로직까지만 작성

--- 자료구조/stackMiro.js
+++ 자료구조/stackMiro.js
@@ -8,3 +8,38 @@ const map = [
   [1,1,1,1,1,1,0]
 ]
 
+const pushLoc = (stack, x, y) => {
+  if (x < 0 || y < 0) {
+    return;
+  }
+
+}
+
+const calculatorValue = (map, size) => {
+  const move = [[1,0], [0,1], [-1,0], [0,-1]];
+  const now = [0,0]
+  const stack = []
+  let lastRocation = null;
+  let answer = 0;
+  while(now[0] !== size-1 && now[1] !== size-1) {
+    
+  }
+  
+  stack.map((rocation) => {
+    if (lastRocation === null) {
+      answer = 100;
+      lastRocation = rocation
+      return;
+    }
+
+    if (lastRocation === rocation) {
+      answer += 100;
+    } else {
+      answer += 500;
+      lastRocation = roaction
+    }
+  })
+  console.log(answer);
+}
+
+calculatorValue(map, map.length)
